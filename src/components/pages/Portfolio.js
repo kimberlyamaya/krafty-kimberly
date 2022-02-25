2c722904ab7e5e98424c348b13abfe572292bafe
kimberlyamaya <kimberly.kimbell89@gmail.com>
2022-02-24 21:10:54 -0800

    added project links to portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,15 +5,30 @@ function Portfolio({currentPage}) {
 console.log(currentPage)
     return (
         <main>
-            <section>
+            <section >
                 <h2>{currentPage}</h2>
-                <div className='container'>
+                <div className='container d-flex justify-content-center'>
                     <div className='row'>
-                        <div className='col'>
-                            <img src='...' alt='project 1'></img>
+                        <div className='col-5 border rounded m-2'>
+                            <a href='/' target='_blank' rel='noreferrer noopener'>
+                                {/* having trouble here */}
+                                <img src='../assets/header/portfolio-background.jpg' alt='project 1'></img>
+                            </a>
                         </div>
-                        <div className='col'>
+                        <div className='col-5 border rounded m-2'>
                             <img src='...' alt='project 2'></img>
+                        </div>
+                        <div className='col-5 border rounded m-2'>
+                            <img src='...' alt='project 3'></img>
+                        </div>
+                        <div className='col-5 border rounded m-2'>
+                            <img src='...' alt='project 4'></img>
+                        </div>
+                        <div className='col-5 border rounded m-2'>
+                            <img src='...' alt='project 5'></img>
+                        </div>
+                        <div className='col-5 border rounded m-2'>
+                            <img src='...' alt='project 6'></img>
                         </div>
                     </div>
                 </div>
