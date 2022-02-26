5a8cddd9f156384ccb134fc6e6b9bad15e063a57
kimberlyamaya <kimberly.kimbell89@gmail.com>
2022-02-26 10:27:07 -0800

    contact form working

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Contact({currentPage}) {
-    // logic for onblur inside function
-    function checkEmail (email) {
-        console.log(email)
 
+    const [formState, setFormState] = useState({name: '', email: '', message:''})
+
+    // const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+      }
+
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log(formState);
     }
 
     return (
         <div className='container'>
-            <div className='d-flex flex-column'>
+            <form className='d-flex flex-column' id="contact-form" onSubmit={handleSubmit}>
                 <h2>{currentPage}</h2>
+
                 <label htmlFor='name'>Name:</label>
-                <input type='text' id='name' name='name'></input>
+                <input type='text' id='name' name='name' defaultValue={formState.name} onChange={handleChange}></input>
+
                 {/* logic: show message if email is invalid */}
                 <label htmlFor='email'>Email Address:</label>
-                <input type='email' id='email' name='email' onBlur={checkEmail}></input>
+                {/* <input type='email' id='email' name='email' onBlur={checkEmail}></input> */}
+                <input type='email' id ='email' name='email' defaultValue={formState.email} onChange={handleChange}></input>
+
                 {/* logic:  show message if message is empty*/}
                 <label htmlFor='message'>Message:</label>
-                <textarea id='message' name='message' rows='4' cols='50'></textarea>
-                {/* logic:  button should send message*/}
-                <button>Submit</button>
-            </div>
+                <textarea id='message' name='message' rows='5' defaultValue={formState.message} onChange={handleChange}></textarea>
+
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
